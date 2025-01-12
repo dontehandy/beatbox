7a5e14c4eb3c7517bac0e720ac6df69579c7ce49
Donte Handy <donte.handy@gmail.com>
2025-01-12 16:21:51 -0400

    Refactor Node class initialization and update tests for clarity

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -3,10 +3,10 @@ require './lib/node'
 
 RSpec.describe Node do
   it 'exists and has attributes' do
-    node = Node.new("plop")
+    node = Node.new("plop") #plop is the argument passed to the new instance of Node
 
     expect(node).to be_an_instance_of(Node)
-    expect(node.data).to eq("plop")
-    expect(node.next_node).to be_nil
+    expect(node.data).to eq("plop") 
+    expect(node.next_node).to be_nil 
   end
 end
