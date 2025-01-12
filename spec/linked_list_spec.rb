5e597274ed90a6ae97fba6b88a2382bc9dbd2e76
Donte Handy <donte.handy@gmail.com>
2025-01-12 17:18:50 -0400

    Update LinkedList spec with new test data for append, prepend, insert, count, and to_string methods

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -10,48 +10,51 @@ RSpec.describe LinkedList do
 
   it 'can append nodes' do
     list = LinkedList.new
-    list.append("doop")
+    list.append("plop")
 
-    expect(list.head.data).to eq("doop")
+    expect(list.head.data).to eq("plop")
     expect(list.head.next_node).to be_nil
 
-    list.append("deep")
-    expect(list.head.next_node.data).to eq("deep")
+    list.append("suu")
+    expect(list.head.next_node.data).to eq("suu")
   end
 
   it 'can prepend nodes' do
     list = LinkedList.new
-    list.append("doop")
-    list.prepend("deep")
+    list.append("plop")
+    list.prepend("dop")
 
-    expect(list.head.data).to eq("deep")
-    expect(list.head.next_node.data).to eq("doop")
+    expect(list.head.data).to eq("dop")
+    expect(list.head.next_node.data). to eq("plop")
   end
 
   it 'can insert nodes at a given position' do
     list = LinkedList.new
-    list.append("doop")
-    list.append("woo")
-    list.insert(1, "deep")
+    list.append("plop")
+    list.append("suu")
+    list.insert(1, "woo")
 
-    expect(list.head.data).to eq("doop")
-    expect(list.head.next_node.data).to eq("deep")
-    expect(list.head.next_node.next_node.data).to eq("woo")
+    expect(list.head.data).to eq("plop")
+    expect(list.head.next_node.data).to eq("woo")
+    expect(list.head.next_node.next_node.data).to eq("suu")
   end
 
   it 'can count nodes' do
     list = LinkedList.new
-    list.append("doop")
-    list.append("deep")
+    list.append("plop")
+    list.append("suu")
+    list.prepend("dop")
 
-    expect(list.count).to eq(2)
+    expect(list.count).to eq(3)
   end
 
   it 'can convert to string' do
     list = LinkedList.new
-    list.append("doop")
-    list.append("deep")
+    list.append("plop")
+    list.append("suu")
+    list.prepend("dop")
+    list.insert(1, "woo")
 
-    expect(list.to_string).to eq("doop deep")
+    expect(list.to_string).to eq("dop woo plop suu")
   end
 end
